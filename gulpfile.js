fa760e3475155a1ab1930f1977b13642bbe4f3a5
Arun Biju <61945450+arunbiju969@users.noreply.github.com>
2025-10-16 14:41:23 -0300

    new file:   .github/chatmodes/GPT 4.1 V3 BeastMode.chatmode.md 	modified:   gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -79,7 +79,7 @@ let shaders = [
 // from the command line to start the server (default port is 8080)
 gulp.task('webserver', gulp.series(async function() {
 	server = connect.server({
-		port: 1234,
+		port: 1235,
 		https: false,
 	});
 }));
